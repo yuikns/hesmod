9de5cb3319af10b7e021ea52e2b583543470867a
Yu Jing <yuikns@users.noreply.github.com>
2019-02-26 16:33:45 -0500

    rename variable from author_link to comment_author_link

--- inc/helpers/layout-functions.php
+++ inc/helpers/layout-functions.php
@@ -154,11 +154,11 @@ if ( ! function_exists( 'hestia_comments_list' ) ) {
 				id="comment-<?php comment_ID(); ?>">
 			<?php if ( $args['type'] != 'pings' ) : ?>
 				<?php
-				$author_link = esc_url( get_comment_author_url( $comment ) );
+				$comment_author_link = esc_url( get_comment_author_url( $comment ) );
 				?>
 				<a class="pull-left" 
-					<?php if (strlen($author_link) > 0) : ?>
-					href="<?php echo $author_link ; ?>" rel="nofollow"
+					<?php if (strlen($comment_author_link) > 0) : ?>
+					href="<?php echo $comment_author_link ; ?>" rel="nofollow"
 					<?php endif; ?>
 					>
 					<div class="comment-author avatar vcard">
