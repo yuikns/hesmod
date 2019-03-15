d95a8cff63567843bc20c1cf181bf22f8c59b748
Yu Jing <yuikns@users.noreply.github.com>
2019-03-15 15:18:23 -0400

    update behavior of clicking the anchor

--- assets/js/patch.js
+++ assets/js/patch.js
@@ -5,5 +5,12 @@ jQuery(document).ready(function($) {
 
 $('argcv').html('<span style="color:MediumPurple;">A</span><span style="color:SkyBlue;">rg</span><span style="color:Gold;">C</span><span style="color:OrangeRed;">V</span>');
 
+// update the behavior of clicking a anchor
+$('.entry-content a').click(function( event ) {
+  event.preventDefault();
+  if($(this).attr('href') && $($(this).attr('href')).offset() && $($(this).attr('href')).offset().top) {
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 200 }, 500);
+  }
+});
 
 });
