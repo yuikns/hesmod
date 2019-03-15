586c89f9a3b9e1282759c22978bb80092e154698
Yu Jing <yuikns@users.noreply.github.com>
2019-03-15 15:38:44 -0400

    bug fix: don't prevent to much behavior

--- assets/js/patch.js
+++ assets/js/patch.js
@@ -7,8 +7,9 @@ $('argcv').html('<span style="color:MediumPurple;">A</span><span style="color:Sk
 
 // update the behavior of clicking a anchor
 $('.entry-content a').click(function( event ) {
-  event.preventDefault();
-  if($(this).attr('href') && $($(this).attr('href')).offset() && $($(this).attr('href')).offset().top) {
+  if($(this).attr('href') && $(this).attr('href').startsWith('#')
+    && $($(this).attr('href')).offset() && $($(this).attr('href')).offset().top) {
+    event.preventDefault();
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 200 }, 500);
   }
 });
